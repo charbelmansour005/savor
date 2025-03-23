b133cde693fcacdbc3e2a51efde06f12be23d411
charbelMansourDev <charbel.mansour@compu-vision.me>
2025-03-23 11:45:35 +0200

    .

--- app/page.tsx
+++ app/page.tsx
@@ -124,7 +124,7 @@ export default function Home() {
             }}
             viewport={{ once: true }}
             whileTap={{ scale: 0.98 }}
-            className="absolute left-50 bottom-15 group border-1 cursor-pointer py-2 px-2 bg-[#000] hover:bg-[#ede3cc] transition duration-300"
+            className="absolute left-1/2 bottom-1/2 transform -translate-x-1/2 -translate-y-1/2 md:bottom-15 md:left-85 group border cursor-pointer py-2 px-2 bg-[#000] hover:bg-[#ede3cc] transition duration-300"
           >
             <p
               className={`${lato.className} select-none text-sm uppercase tracking-[2] font-normal text-[#ede3cc] group-hover:text-[#000000]`}
@@ -134,6 +134,7 @@ export default function Home() {
           </motion.button>
         </Swiper>
       </div>
+      {/* Swiper end */}
 
       <div
         style={{ backgroundColor: Colors.sandDark }}
